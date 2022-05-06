6161e78f1a70a83c01f8ae5c22aa6db62944feff
richinarm <60946200+richinarm@users.noreply.github.com>
2022-05-06 15:02:04 +0000

    added Jack Model to scene

--- ML_Testing.py
+++ ML_Testing.py
@@ -3,7 +3,9 @@ from direct.showbase.ShowBase import ShowBase
 class MainGame(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
-
+        self.player = self.loader.loadModel('models/jack')
+        self.player.reparentTo(self.render)
+        self.cam.setPos(0, -20, 0)
 
 
 testgame = MainGame()
